9ed642c0a38e9be44085aa3de0337bc41c075121
ocramz <zocca.marco@gmail.com>
2020-04-09 11:18:44 +0200

    add Data.Align.Text

--- src/Data/Align/Demo.hs
+++ src/Data/Align/Demo.hs
@@ -12,19 +12,24 @@ sampleGlobalConfig = alignConfig
   (-0.5)
   (-1)
 
+testIn1, testIn2 :: String
 testIn1 = "dopple"
 testIn2 = "applied"
 
+alignedGlobal :: Trace Char Double
 alignedGlobal =
   align sampleGlobalConfig (V.fromList testIn1) (V.fromList testIn2)
 
+debug :: Trace Char s -> IO ()
 debug = putStrLn . debugAlign . trace
 
+printAlignedGlobal :: IO ()
 printAlignedGlobal = debug alignedGlobal
 
 -- * Multi-sequence fun.
 
 -- | Example from https://www.biostat.wisc.edu/bmi576/lectures/multiple-alignment.pdf
+nucs :: [String]
 nucs =
   [ "ATTGCCATT"
   , "ATGGCCATT"
@@ -33,15 +38,20 @@ nucs =
   , "ATTGCCGATT"
   ]
 
+alignNuc :: Eq a => [a] -> [a] -> Trace a Double
 alignNuc a b = align sampleGlobalConfig (V.fromList a) (V.fromList b)
 
+alignedNucPairs :: [Trace Char Double]
 alignedNucPairs = do
   n <- tail nucs
   return $ alignNuc (head nucs) n
 
+printAlignedNucPairs :: IO ()
 printAlignedNucPairs = mapM_ (\x -> debug x >> putStrLn "") alignedNucPairs
 
+alignedNucStar :: MultiTrace Integer Char Double
 alignedNucStar =
   centerStar sampleGlobalConfig $ zip [1..] (map V.fromList nucs)
 
+printAlignedNucStar :: IO ()
 printAlignedNucStar = putStrLn . debugMultiAlign . multiTrace $ alignedNucStar
